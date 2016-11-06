6ceb9bfdd0aa50e4c45becb25d1b123d2ee78744
Shangtong Zhang <zhangshangtong.cpp@icloud.com>
2016-11-06 09:31:38 -0700

    Fix a bug about hashtag

--- js/controller.js
+++ js/controller.js
@@ -1,14 +1,34 @@
 var app = angular.module('RL', []);
-app.controller('RLController', ['$anchorScroll', '$location', '$scope',
-    function($anchorScroll, $location, $scope) {
-    var onDataAvailable = function(response) {
-        $scope.figures = response
-        $anchorScroll($location.hash())
+app.directive('imageonload', function() {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+            element.bind('load', function() {
+                scope.$apply(attrs.imageonload);
+            });
+        }
     };
-    $.ajax({
-        url: 'data/data.json',
-        async: false,
-        dataType: 'json',
-        success: onDataAvailable
-    })
+});
+app.controller('RLController', ['$anchorScroll', '$location', '$timeout', '$window', '$scope',
+    function($anchorScroll, $location, $timeout, $window, $scope) {
+        var totalImages = 0;
+        var loadedImages = 0;
+        $scope.imageLoaded = function() {
+            loadedImages += 1;
+            if (loadedImages == totalImages) {
+                $anchorScroll($location.url().substring(1));
+            }
+        };
+        var onDataAvailable = function(response) {
+            $scope.figures = response;
+            $scope.figures.forEach(function(figure) {
+                totalImages += figure.imageNames.length
+            });
+        };
+        $.ajax({
+            url: 'data/data.json',
+            async: false,
+            dataType: 'json',
+            success: onDataAvailable
+        });
 }]);
